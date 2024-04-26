3e8b07bb6a6c72647bf0fcaf4edc9a7e75cc2c91
Kang <63038044+pstpmn@users.noreply.github.com>
2024-04-26 23:06:06 +0700

    test : add test-case

--- main_test.go
+++ main_test.go
@@ -23,6 +23,16 @@ func Test_sum(t *testing.T) {
 			args: args{0, 4},
 			want: 4,
 		},
+		{
+			name: "should be return 4",
+			args: args{5, 4},
+			want: 4,
+		},
+		{
+			name: "should be return 10",
+			args: args{5, 5},
+			want: 10,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
